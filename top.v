e82282c32f248d3187b90c7722c2282cde640614
Neil Nie <yongyang.nie@gmail.com>
2019-04-15 10:52:35 -0400

    minute changing button works

--- top.v
+++ top.v
@@ -1,6 +1,6 @@
 // look in pins.pcf for all the pin names on the TinyFPGA BX board
 module top (
-  input CLK,      // 16MHz clock
+  input CLK,       // 16MHz clock
   output PIN_1,    // PIN_1
   output PIN_2,
   output PIN_3,
@@ -22,15 +22,8 @@ module top (
   reg [3:0] display_in;
 
   // regs that hold values for each digit of the clock.
-  reg [3:0] minute_one;
-  reg [3:0] minute_two;
-  reg [3:0] hour_one;
-  reg [3:0] hour_two;
-
-  // user sets the minut and hour offset, which will be added
-  // or subtracted from the minute or hour count.
-  reg [3:0] minute_offset;
-  reg [3:0] hour_offset;
+  reg sec_ind;
+  reg [15:0] time_count;
 
   // the ground pins for the four seven segment displays.
   // those pins will be switch on and off to display different
@@ -46,77 +39,69 @@ module top (
   reg [3:0] display_select_counter;
 
   initial begin
+    time_count <= 0;
+    sec_ind <= 0;
     blink_counter <= 0;
-    minute_one <= 4'b0000;
-    minute_two <= 4'b0000;
-    hour_one <= 4'b0000;
-    hour_two <= 4'b0000;
-    minute_offset <= 4'b0000;
-    hour_offset <= 4'b0000;
     display_select_counter <= 0;
   end
 
-  always @(negedge PIN_11) begin
-
-  end
-
-  debounce db(.clk(CLK), .PB(PIN_10), .PB_state(PIN_10_db));
-
-  always @ (posedge PIN_10_db) begin
-    minute_offset <= minute_offset + 1;
-  end
+  debounce db(.clk(CLK), .PB(~PIN_10), .PB_state(PIN_10_db));
 
   // increment the blink_counter every clock
   always @(posedge CLK) begin
 
-      hour_one = hour_one + minute_offset;
-      if (blink_counter % 16000000 == 0) begin
-        if (minute_one < 9) begin
-          minute_one <= minute_one + 1;
-        end else if (minute_one == 9 && minute_two < 5) begin
-          minute_one <= 4'b0000;
-          minute_two <= minute_two + 1;
-        end else if (minute_one == 9 && minute_two == 5) begin
-          hour_one <= hour_one + 1;
-          minute_one <= 4'b0000;
-          minute_two <= 4'b0000;
+      if (blink_counter % 2500000 == 0) begin
+        if (PIN_10_db == 1) begin
+          time_count <= time_count + 60;
         end
+      end
+
+      if (blink_counter % 10000000 == 0) begin
+        time_count <= time_count + 1;
+        // sec_ind = ~sec_ind;
       end
 
       if (blink_counter % 500 == 0) begin
+
         if (display_select_counter == 0) begin
-          gnd_1 = 0;
-          gnd_2 = 1;
-          gnd_3 = 1;
-          gnd_4 = 1;
-          display_in <= hour_two;
-          display_select_counter <= display_select_counter + 1;
+        gnd_1 = 0;
+        gnd_2 = 1;
+        gnd_3 = 1;
+        gnd_4 = 1;
+        display_in <= (time_count / 3600) / 10;
+        display_select_counter <= display_select_counter + 1;
         end else if (display_select_counter == 1) begin
-          gnd_1 = 1;
-          gnd_2 = 0;
-          gnd_3 = 1;
-          gnd_4 = 1;
-          display_in <= minute_offset;
-          display_select_counter <= display_select_counter + 1;
+
+        gnd_1 = 1;
+        gnd_2 = 0;
+        gnd_3 = 1;
+        gnd_4 = 1;
+        display_in <= (time_count / 3600) % 10;
+        display_select_counter <= display_select_counter + 1;
+
         end else if (display_select_counter == 2) begin
-          gnd_1 = 1;
-          gnd_2 = 1;
-          gnd_3 = 0;
-          gnd_4 = 1;
-          display_in <= minute_two;
-          display_select_counter <= display_select_counter + 1;
+
+        gnd_1 = 1;
+        gnd_2 = 1;
+        gnd_3 = 0;
+        gnd_4 = 1;
+        display_in <= (time_count / 60) / 10;
+        display_select_counter <= display_select_counter + 1;
+
         end else if (display_select_counter == 3) begin
-          gnd_1 = 1;
-          gnd_2 = 1;
-          gnd_3 = 1;
-          gnd_4 = 0;
-          display_in <= minute_one;
-          display_select_counter <= 0;
+
+        gnd_1 = 1;
+        gnd_2 = 1;
+        gnd_3 = 1;
+        gnd_4 = 0;
+        display_in <= (time_count / 60) % 10;
+        display_select_counter <= 0;
+
         end
+        
       end
 
       blink_counter <= blink_counter + 1;
-
   end
 
   assign PIN_20 = gnd_1;
@@ -134,6 +119,6 @@ module top (
   assign PIN_6 = display_out[1];
   assign PIN_7 = display_out[0];
 
-  assign PIN_8 = PIN_10;
+  // assign PIN_8 = sec_ind;
 
   endmodule
